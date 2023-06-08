ec93ff1f7fdd7051badee5cafb16fe3953bdedc9
Prasad Chathuranga <62802167+ChathurangaMMP@users.noreply.github.com>
2023-06-09 03:26:15 +0530

    Update fastapi_app_skype.py by removing slack ID.

--- main_program/fastapi_app_skype.py
+++ main_program/fastapi_app_skype.py
@@ -5,11 +5,6 @@ from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings
 from botbuilder.schema import Activity
 
 
-#  secret ID - b67916cf-b40c-40b0-92e0-f44fe4cd4d95
-# ngrok authtoken - 2Q5ZhI4w9jKLYcK7qZtMngwuq0R_26BDNyEk6o5FGyxVp4mAd
-# temp url - https://c5f7-2407-c00-d004-5c51-6abf-c46c-41f3-49a0.ngrok-free.app
-# slack app token - xoxb-5300103418852-5294684594885-9K580jYrkK0OXFUZzJuiGozJ
-
 class MyBot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         text = turn_context.activity.text
